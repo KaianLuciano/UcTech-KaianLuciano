3c52af59957737a333e55be97d26b9a8628b1a61
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2023-06-28 19:22:59 -0300

    Implementado Método HTTP Delete

--- src/main/java/com/uctech/desafio/controller/EmpresaController.java
+++ src/main/java/com/uctech/desafio/controller/EmpresaController.java
@@ -4,6 +4,7 @@ import com.uctech.desafio.model.EmpresaModel;
 import com.uctech.desafio.service.CnpjService;
 import com.uctech.desafio.service.EmpresaService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.repository.config.RepositoryNameSpaceHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +19,8 @@ public class EmpresaController {
     final EmpresaService empresaService;
 
     //Método utilizado para testes
-    @GetMapping("/{cnpj}")
-    public ResponseEntity<EmpresaModel> consultarEmpresa(@PathVariable(value = "cnpjEmpresa") String cnpj) {
+    @GetMapping("/consultar/api/{cnpj}")
+    public ResponseEntity<EmpresaModel> consultarEmpresa(@PathVariable(value = "cnpj") String cnpj) {
 
         EmpresaModel empresa = cnpjService.retornaEmpresa(cnpj);
         return ResponseEntity.ok(empresa);
@@ -30,9 +31,19 @@ public class EmpresaController {
         return ResponseEntity.status(HttpStatus.OK).body(empresaService.findAll());
     }
 
+    @GetMapping("/{cnpj}")
+    ResponseEntity<Object> findById(@PathVariable(value = "cnpj") String cnpj) {
+        return ResponseEntity.status(HttpStatus.OK).body(empresaService.findById(cnpj));
+    }
+
     @PostMapping("/{cnpj}")
-    public ResponseEntity<Object> save(@PathVariable(value = "cnpj") String cnpj) {
+    public ResponseEntity<Object> saveEmpresa(@PathVariable(value = "cnpj") String cnpj) {
         return ResponseEntity.status(HttpStatus.OK).body(empresaService.saveEmpresa(cnpj));
+    }
+
+    @DeleteMapping("/{cnpj}")
+    public ResponseEntity<Object> deleteById(@PathVariable String cnpj) {
+        return ResponseEntity.status(HttpStatus.OK).body(empresaService.deleteEmpresa(cnpj));
     }
 
 }
--- src/main/java/com/uctech/desafio/service/EmpresaService.java
+++ src/main/java/com/uctech/desafio/service/EmpresaService.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,6 +18,19 @@ public class EmpresaService {
 
     final CnpjService cnpjService;
 
+
+    public EmpresaModel findById(String cnpj) {
+
+        /*
+        Fazer validações com Optional como isPresent
+         */
+
+        String cnpjFormatado = cnpj.replaceAll("[\\.-]", "");
+
+        Optional<EmpresaModel> empresaModel = empresaRepository.findById(cnpjFormatado);
+
+        return empresaModel.get();
+    }
 
     public List<EmpresaModel> findAll(){
         List<EmpresaModel> empresas = empresaRepository.findAll();
@@ -33,5 +47,19 @@ public class EmpresaService {
         EmpresaModel empresa = cnpjService.retornaEmpresa(cnpj);
         empresaRepository.save(empresa);
         return "Salvo com sucesos";
+    }
+
+    @Transactional
+    public String deleteEmpresa(String cnpj) {
+
+        /*
+        Fazer validação necessarias
+        Formatar o cnpj caso possua [. -] para deletar a empresa sem grandes problemas
+         */
+
+        String cnpjFormatado = cnpj.replaceAll("[\\.-]", "");
+
+        empresaRepository.deleteById(cnpjFormatado);
+        return "Produto Deletado!!";
     }
 }
