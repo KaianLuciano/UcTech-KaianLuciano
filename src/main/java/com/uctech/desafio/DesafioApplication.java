b62286e4f27d62376f2321241deb7225765f5f82
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2023-06-29 20:58:32 -0300

    Adicionado Documentação Swagger

--- src/main/java/com/uctech/desafio/DesafioApplication.java
+++ src/main/java/com/uctech/desafio/DesafioApplication.java
@@ -1,8 +1,11 @@
 package com.uctech.desafio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "UcTech", version = "1", description = "API Desenvolvida para o Gerenciamento de Dados recebidos"))
 @SpringBootApplication
 public class DesafioApplication {
 
--- src/main/java/com/uctech/desafio/controller/EmpresaController.java
+++ src/main/java/com/uctech/desafio/controller/EmpresaController.java
@@ -1,8 +1,9 @@
 package com.uctech.desafio.controller;
 
-import com.uctech.desafio.model.EmpresaModel;
 import com.uctech.desafio.service.CnpjService;
 import com.uctech.desafio.service.EmpresaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/empresas")
+@Tag(name = "Empresa")
 @AllArgsConstructor
 public class EmpresaController {
 
@@ -17,26 +19,31 @@ public class EmpresaController {
 
     final EmpresaService empresaService;
 
+    @Operation(summary = "Irá buscar todas as empresas contidas no banco")
     @GetMapping
     public ResponseEntity<Object> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(empresaService.findAll());
     }
 
+    @Operation(summary = "Retorna o carrinho que representa o CNPJ fornecido")
     @GetMapping("/{cnpj}")
     ResponseEntity<Object> findById(@PathVariable(value = "cnpj") String cnpj) {
         return ResponseEntity.status(HttpStatus.OK).body(empresaService.findById(cnpj));
     }
 
+    @Operation(summary = "Retorna a empresa que representa a RazãoSocial")
     @GetMapping("/nome/{razaoSocial}")
-    ResponseEntity<Object> findEmpresaModelByNomeFantasia(@PathVariable(value = "razaoSocial") String razaoSocial) {
+    ResponseEntity<Object> findEmpresaModelByRazaoSocial(@PathVariable(value = "razaoSocial") String razaoSocial) {
         return ResponseEntity.status(HttpStatus.OK).body(empresaService.findEmpresaModelByRazaoSocial(razaoSocial));
     }
 
+    @Operation(summary = "Salva a empresa no banco que representa o CNPJ fornecido, sendo o mesmo consultando em uma API publica")
     @PostMapping("/{cnpj}")
     public ResponseEntity<Object> saveEmpresa(@PathVariable(value = "cnpj") String cnpj) {
         return ResponseEntity.status(HttpStatus.OK).body(empresaService.saveEmpresa(cnpj));
     }
 
+    @Operation(summary = "Deleta a empresa representada pelo CNPJ usada para Testes")
     @DeleteMapping("/{cnpj}")
     public ResponseEntity<Object> deleteById(@PathVariable String cnpj) {
         return ResponseEntity.status(HttpStatus.OK).body(empresaService.deleteEmpresa(cnpj));
