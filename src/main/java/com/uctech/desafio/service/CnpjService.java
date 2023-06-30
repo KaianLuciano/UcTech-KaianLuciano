d7f161a0c4685924d53f036bb5e91c9a41322dba
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2023-06-30 10:42:30 -0300

    Otimização Código

--- src/main/java/com/uctech/desafio/service/CnpjService.java
+++ src/main/java/com/uctech/desafio/service/CnpjService.java
@@ -6,19 +6,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class CnpjService {
-
     public EmpresaModel retornaEmpresa(String cnpj) {
 
         String url = "https://brasilapi.com.br/api/cnpj/v1/";
         String uri = "https://brasilapi.com.br/api/cnpj/v1/{cnpj}";
 
-        EmpresaModel empresa = WebClient
+        return  WebClient
                 .create(url)
                 .get()
                 .uri(uri, cnpj)
                 .retrieve()
                 .bodyToMono(EmpresaModel.class).block();
 
-        return empresa;
     }
 }
